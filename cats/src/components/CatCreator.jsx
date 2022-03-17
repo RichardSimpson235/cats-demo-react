7986ef56f9eb00b8a703fda2e55148486d0b30a4
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-17 14:09:37 -0400

    Implement CatCreator

--- cats/src/components/CatCreator.jsx
+++ cats/src/components/CatCreator.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react'
+import { Box, Button, TextField } from "@mui/material"
+import axios from 'axios'
+
 function CatCreator() {
+
+    const [name, setName] = useState("")
+    const [breed, setBreed] = useState("")
+    const [cat, setCat] = useState(null)
+
+    const createCat = async () => {
+        const response = await axios.post("http://localhost:8080/cats/", {
+            name: name,
+            breed: breed
+        })
+
+        const data = await response.data
+        setCat(data)
+    }
+
     return (
         <>
-            <p>I can create cats!</p>
+            <TextField
+                label="Name"
+                onChange={e => setName(e.target.value)}
+            />
+            <TextField
+                label="Breed"
+                onChange={e => setBreed(e.target.value)}
+            />
+            <Button variant="outlined" onClick={createCat}>Create</Button>
+            {
+                cat && <p>Your new cat's id is: {cat.id}!</p>
+            }
         </>
     )
 }
