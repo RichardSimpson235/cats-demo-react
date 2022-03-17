312726271731d1e69ea00cd94f0aadf9d3df1f1f
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-17 12:27:11 -0400

    Add routing

--- cats/src/index.js
+++ cats/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom"
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
